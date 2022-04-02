06e133ae4ef2a8232f5ac2c5ad9a62708a45c60b
Dante Bazaldua <dante@spot2.mx>
2022-04-02 00:42:27 -0600

    [ADDED] Fixed color count

--- tests.py
+++ tests.py
@@ -5,10 +5,10 @@ def rgb_to_hex(r, g, b):
     return '#{:X}{:X}{:X}'.format(r, g, b)
 
 
-def get_palette(path, color_count, hex=True):
+def get_palette(path, color_count=6, hex=True):
     color_thief = ColorThief(path)
     # get the dominant color
-    palette_rgb = color_thief.get_palette(color_count=6)
+    palette_rgb = color_thief.get_palette(color_count)
     if hex is True:
         palette_hex = []
         for color in palette_rgb:
